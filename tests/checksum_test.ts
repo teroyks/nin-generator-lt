dc8443fdf715474d794aa328410090ad608650b8
Tero Ykspetäjä <tero.ykspetaja@tact.fi>
2021-02-12 22:21:45 +0200

    ✅ fix test import
    Forgot to rename the checksum function in test.

--- tests/checksum_test.ts
+++ tests/checksum_test.ts
@@ -1,9 +1,9 @@
 import { assertEquals } from "asserts";
-import { ltNINChecksum } from "../src/lt-nin-rules.ts";
+import { checksum } from "../src/lt-nin-rules.ts";
 
 /**
  * 10101010005 is a valid NIN.
  */
 Deno.test("calculate checksum", () => {
-  assertEquals(ltNINChecksum("1010101000"), 5);
+  assertEquals(checksum("1010101000"), 5);
 });
